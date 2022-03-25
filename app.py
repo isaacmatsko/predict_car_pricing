cc47f72844033c93163f6516f7b5adcfe1fa3bb0
astroshane <roshane.bent@eastern.edu>
2022-03-25 11:15:43 -0400

    updated routes

--- app.py
+++ app.py
@@ -4,6 +4,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def homepage():
+@app.route("/isaac")
+def issac():
     return render_template("resume_isaac.html")
+
+@app.route("/sean")
+def sean():
+    return render_template("resume_sean.html")
+
+@app.route("/alec")
+def alec():
+    return render_template("resume_alec.html")
+
+@app.route("/shane")
+def shane():
+    return render_template("resume_shane.html")
+
+
